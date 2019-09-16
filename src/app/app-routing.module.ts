3e0e450c9ac02e04bd1ecf512202231db2e928be
Damian <stojaniuk.damian@gmail.com>
2019-09-16 12:27:53 +0200

    Auth changes, add token decoding and nav dropdown

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
 import { ValuesComponent } from './values/values.component';
 
 const appRoutes: Routes = [
--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, Validators, FormControl } from '@angular/forms';
-import { HttpClient } from '@angular/common/http';
+import { AuthService } from 'src/app/services/auth.service';
+import { AlertifyService } from 'src/app/services/alertify.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -9,27 +11,30 @@ import { HttpClient } from '@angular/common/http';
 })
 export class LoginComponent implements OnInit {
   loginForm: FormGroup;
-  baseUrl = 'https://localhost:5001/auth/login/';
 
-  constructor(private http: HttpClient) {}
+  constructor(private authService: AuthService, private alertify: AlertifyService, private router: Router) {}
 
   ngOnInit() {
     this.loginForm = new FormGroup({
       userData: new FormGroup({
-        email: new FormControl(null, [Validators.required]),
+        email: new FormControl(null, [Validators.required, Validators.email]),
         password: new FormControl(null, [Validators.required]),
       })
     });
   }
 
   onSubmit() {
-    console.log(this.loginForm);
     this.login(this.loginForm.value.userData);
+    this.loginForm.reset();
   }
 
   private login(model: any) {
-    this.http.post(this.baseUrl, model).subscribe(error => {
-      console.log(error);
+    this.authService.login(model).subscribe(next => {
+      this.alertify.success('Zalogowano!');
+    }, error => {
+      this.alertify.error(error);
+    }, () => {
+      this.router.navigate(['/users']);
     });
   }
 }
